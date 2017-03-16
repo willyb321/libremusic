e62d799f53d9d6b24a07883235a56c4d02f98cfd
Ed Jones <info@edjon.es>
2017-03-16 13:56:42 +1100

    Initial upload
    Contains a basic progress bar + a basic search bar

---
+++ script.js
@@ -0,0 +1,150 @@
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+	player = new YT.Player('player', {
+		height: '390',
+		width: '640',
+		events: {
+			'onReady': onPlayerReady,
+			'onStateChange': onPlayerStateChange
+		}
+	});
+
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+	// changeSource()
+	// event.target.playVideo();
+
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+	/*if (event.data == YT.PlayerState.PLAYING && !done) {
+		setTimeout(stopVideo, 6000);
+		done = true;
+	}*/
+	if (player.getPlayerState() == 1) {
+    	document.getElementById( "title" ).innerText = player.getVideoData().title;
+    } 
+
+}
+
+
+// This function takes in a YouTube video id and changes 
+// the video on the page to this video
+function changeSource(source) {
+	var source = prompt('Video Id?');
+	player.loadVideoById(source);
+	progressBar();
+	togglePlay();
+
+}
+
+
+// This function defines the rules for the progress bar
+function progressBar(progress, total) {
+	var prog = document.getElementById("myBar");
+	var load = document.getElementById("loadBar");
+	setInterval(() => {
+		prog.style.width = player.getCurrentTime() / player.getDuration() * 100 + '%';
+		load.style.width = player.getVideoLoadedFraction() * 100 + '%';
+		if (player.getVideoLoadedFraction() > 0) {
+			document.getElementById("progress").value = player.getCurrentTime()*100000/player.getDuration();
+		} else {
+			document.getElementById("progress").value = 0;
+		}
+	}, 300);
+
+
+}
+
+
+// These functions change video settings when called
+function fastForward(){
+	player.seekTo(player.getCurrentTime() + 10);
+}
+function rewind(){
+	player.seekTo(player.getCurrentTime() - 10);
+}
+function volumeUp(){
+	player.setVolume(player.getVolume() + 10);	
+}
+function volumeDown(){
+	player.setVolume(player.getVolume() - 10);	
+}
+function seek(ts){
+	player.seekTo(ts.value/100000*player.getDuration(), true);
+}
+
+// These functions toggle video settings and change textures
+function toggleMute(){
+	if (player.isMuted()) {
+		player.unMute()	
+		document.getElementById("toggleMuteButton").src = "icons/loud.svg"
+	} else {
+		player.mute()	
+		document.getElementById("toggleMuteButton").src = "icons/mute.svg"
+	}
+}
+function togglePlay(){
+	if (player.getPlayerState() == 1) {
+		player.pauseVideo()	
+		document.getElementById("playPauseButton").src = "icons/play.svg"
+	} else {
+		player.playVideo()	
+		document.getElementById("playPauseButton").src = "icons/pause.svg"
+	}
+}
+
+
+// This function manages hotkeys
+document.onkeydown= function (e) {
+	e = e || window.event;
+
+    if (e.keyCode == '32' || e.keyCode == '75') { // SPACE
+    	togglePlay()
+    }
+    if (e.keyCode == '37' || e.keyCode == '74'){ // LEFT
+    	rewind();
+    }
+    if (e.keyCode == '39' || e.keyCode == '76'){ // RIGHT
+    	fastForward();
+    }
+    if (e.keyCode == '38'){ // UP
+    	volumeUp();
+    }
+    if (e.keyCode == '40'){ // DOWN
+    	volumeDown();
+    }
+    if (e.keyCode == '77'){ // M
+    	toggleMute();
+    }
+};
+
+
+// This function takes a video search query as input
+// and changes the video to the first YouTube result
+function searchVideo(query){
+	togglePlay();
+	console.log(query);
+	player.loadPlaylist({
+		listType:"search",
+		list:query,
+		index:0
+	})
+	progressBar();
+	document.getElementById("history").innerHTML += "<a href='javascript:searchVideo(\"" + query + "\")'>" + query + "</a><br>";
+}
