ccb8ba11ba692d3a565dbdaddf2e2b08dc7ef17a
Ed Jones <edjones1999@gmail.com>
2017-06-10 19:54:38 +1000

    Improved adherence to standard javascript style

--- api.js
+++ api.js
@@ -1,179 +1,163 @@
-const api_key = "39b6e795e0e08c0448bb80df33231284"
+const apiKey = '39b6e795e0e08c0448bb80df33231284'
 
-function searchTrack(query) {
-	query = query.replace(" ", "+");
-	return $.ajax({
-		url: "https://ws.audioscrobbler.com/2.0/?method=track.search&track=" + query + "&api_key=" + api_key + "&format=json"
-	})
+function searchTrack (query) {
+  query = query.replace(' ', '+')
+  return $.ajax({
+    url: 'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + query + '&api_key=' + apiKey + '&format=json'
+  })
 }
 
-function getTrackInfo(artist, track) {
-	return $.ajax({
-		url: "https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=" + api_key + "&artist=" + artist + "&track=" + track + "&format=json"
-	});
-}
-
-
-function getTopTracks(artist, track) {
-	return $.ajax({
-		url: "https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=" + api_key + "&format=json"
-	});
-}
-
-function loadSong(query) {
-	searchTrack(query)
-		.then(function (data) {
-
-			const artist = data.results.trackmatches.track[0].artist;
-			const name = data.results.trackmatches.track[0].name;
-			document.getElementById("title").innerText = name + " - " + artist;
-
-			for (n=0;n<$("#favourites")[0].children.length;n++) {
-				console.log("about to work out your opinion");
-				if ($("#favourites")[0].children[n].lastChild.innerText == name + "\n" + artist){
-					console.log("This song is one of your favourites!");
-					document.getElementById("favourite").src = "icons/favouritegold.svg";
-					break;
-				} else {
-					console.log("I guess you don't like this song that much");
-					document.getElementById("favourite").src = "icons/favourite.svg";
-				}
-			}
-
-
-			getTrackInfo(artist, name)
-				.then(function (data) {
-
-					var mbid = 'h' + makeID(artist, name);
-					console.log(mbid);
-					var albumURL = data.track.album.image[2]["#text"];
-
-					$("." + mbid).remove();
-
-
-					imgsrcToB64(albumURL)
-						.then(art => {
-							document.getElementById("album").src = art;
-
-
-							document.getElementById("history").innerHTML =
-								"<a class='card " + mbid + "' href='javascript:searchVideo(\"" + name.replace("'", "") + " - " + artist + "\")'>" +
-								"<img class='album' src='" + art + "'>" +
-								"<br>" +
-								"<div class='subtitle'>" + name + "<br>" + artist + "</div>" +
-								"</a>" +
-								document.getElementById("history").innerHTML;
-
-							localStorage.setItem("history", document.getElementById("history").innerHTML);
-
-						})
-
-
-
-
-				})
-				.catch(function (data) {
-					var mbid = 'h' + makeID(artist, name);
-					console.log(mbid)
-					$("." + mbid).remove();
-					console.error("Could not find album art for " + query);
-
-					if ($("." + mbid.replace("h", "p")) != null) {
-						art = $("." + mbid.replace("h", "p"))[0].firstChild.src;
-					} else {
-						art = "album.png"
-					}
-					document.getElementById("album").src = art;
-
-
-					document.getElementById("history").innerHTML =
-						"<a class='card " + mbid + "' href='javascript:searchVideo(\"" + name.replace("'", "") + " - " + artist + "\")'>" +
-						"<img class='album' src='" + art + "'>" +
-						"<br>" +
-						"<div class='subtitle'>" + name + "<br>" + artist + "</div>" +
-						"</a>" +
-						document.getElementById("history").innerHTML;
-
-					localStorage.setItem("history", document.getElementById("history").innerHTML);
-				})
-
-
-
-
-		})
+function getTrackInfo (artist, track) {
+  return $.ajax({
+    url: 'https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=' + apiKey + '&artist=' + artist + '&track=' + track + '&format=json'
+  })
 }
 
 
-function addToFavourites() {
-
-	if (player.getPlayerState() == 1 || player.getPlayerState() == 2){
-
-		searchTrack(document.getElementById("title").innerText)
-		.then(function (data) {
-
-			// Define artist and name from API
-			const artist = data.results.trackmatches.track[0].artist;
-			const name = data.results.trackmatches.track[0].name;
-
-			// Get album art url from loaded album art
-			art = document.getElementById("album").src;
-			// Create unique song ID
-			var mbid = 'f' + makeID(artist, name);
-			// Remove existing songs with the same ID
-			$("." + mbid).remove();
-
-			// Add new favourites card
-			document.getElementById("favourites").innerHTML =
-				"<a class='card " + mbid + "' href='javascript:searchVideo(\"" + name.replace("'", "") + " - " + artist + "\")'>" +
-				"<img class='album' src='" + art + "'>" +
-				"<br>" +
-				"<div class='subtitle'>" + name + "<br>" + artist + "</div>" +
-				"</a>" +
-				document.getElementById("favourites").innerHTML;
-
-
-			if ((document.getElementById("favourite").src).endsWith("icons/favourite.svg")) {
-				document.getElementById("favourite").src = "icons/favouritegold.svg"
-			} else if ((document.getElementById("favourite").src).endsWith("icons/favouritegold.svg")) {
-				document.getElementById("favourite").src = "icons/favourite.svg"
-				$("." + mbid).remove();
-			}
-
-			// Save new favourites
-			localStorage.setItem("favourites", document.getElementById("favourites").innerHTML);
-		})
-
-	}
-
-
-
-
+function getTopTracks (artist, track) {
+  return $.ajax({
+    url: 'https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=' + apiKey + '&format=json'
+  })
 }
 
-function loadTopTracks() {
+function loadSong (query) {
+  searchTrack(query)
+    .then(function (data) {
 
-	getTopTracks()
-		.then(function (data) {
-			html = '';
-			for (z = 0; z < 16; z++) {
-				name = data.tracks.track[z].name;
-				artist = data.tracks.track[z].artist.name;
-				art = data.tracks.track[z].image[2]["#text"];
+      const artist = data.results.trackmatches.track[0].artist
+      const name = data.results.trackmatches.track[0].name
+      document.getElementById('title').innerText = name + ' - ' + artist
 
-				var mbid = 'p' + makeID(artist, name);
+      for (var n = 0; n < $('#favourites')[0].children.length; n++) {
+        if ($('#favourites')[0].children[n].lastChild.innerText === name + '\n' + artist) {
+          document.getElementById('favourite').src = 'icons/favouritegold.svg'
+          break
+        } else {
+          document.getElementById('favourite').src = 'icons/favourite.svg'
+        }
+      }
 
 
-				html = html +
-					"<a class='card " + mbid + "' href='javascript:searchVideo(\"" + name.replace("'", "") + " - " + artist + "\")'>" +
-					"<img class='album' src='" + art + "'>" +
-					"<br>" +
-					"<div class='subtitle'>" + name + "<br>" + artist + "</div>" +
-					"</a>";
+      getTrackInfo(artist, name)
+        .then(function (data) {
 
-			}
+          var mbid = 'h' + makeID(artist, name)
+          console.log(mbid)
+          var albumURL = data.track.album.image[2]['#text']
 
-			document.getElementById("popular").innerHTML = html;
+          $('.' + mbid).remove()
 
-		})
 
+          imgsrcToB64(albumURL)
+            .then(art => {
+              document.getElementById('album').src = art
+              document.getElementById('history').innerHTML =
+                "<a class='card " + mbid + "' href='javascript:searchVideo(\"" + name.replace("'", '') + ' - ' + artist + "\")'>" +
+                "<img class='album' src='" + art + "'>" +
+                '<br>' +
+                "<div class='subtitle'>" + name + '<br>' + artist + '</div>' +
+                '</a>' +
+                document.getElementById('history').innerHTML
+
+              localStorage.setItem('history', document.getElementById('history').innerHTML)
+            })
+        })
+        .catch(function (data) {
+          var mbid = 'h' + makeID(artist, name)
+          console.log(mbid)
+          $('.' + mbid).remove()
+          console.error('Could not find album art for ' + query)
+
+          if ($('.' + mbid.replace('h', 'p')) != null) {
+            art = $('.' + mbid.replace('h', 'p'))[0].firstChild.src
+          } else {
+            art = 'album.png'
+          }
+          document.getElementById('album').src = art
+
+
+          document.getElementById('history').innerHTML =
+            "<a class='card " + mbid + "' href='javascript:searchVideo(\"" + name.replace("'", '') + ' - ' + artist + "\")'>" +
+            "<img class='album' src='" + art + "'>" +
+            '<br>' +
+            "<div class='subtitle'>" + name + '<br>' + artist + '</div>' +
+            '</a>' +
+            document.getElementById('history').innerHTML
+
+          localStorage.setItem('history', document.getElementById('history').innerHTML)
+        })
+    })
+}
+
+
+function addToFavourites () {
+
+  if (player.getPlayerState() === 1 || player.getPlayerState() === 2) {
+
+    searchTrack(document.getElementById('title').innerText)
+      .then(function (data) {
+        // Define artist and name from API
+        const artist = data.results.trackmatches.track[0].artist
+        const name = data.results.trackmatches.track[0].name
+
+        // Get album art url from loaded album art
+        var art = document.getElementById('album').src
+        // Create unique song ID
+        var mbid = 'f' + makeID(artist, name)
+        // Remove existing songs with the same ID
+        $('.' + mbid).remove()
+
+        // Add new favourites card
+        document.getElementById('favourites').innerHTML =
+          "<a class='card " + mbid + "' href='javascript:searchVideo(\"" + name.replace("'", '') + ' - ' + artist + "\")'>" +
+          "<img class='album' src='" + art + "'>" +
+          '<br>' +
+          "<div class='subtitle'>" + name + '<br>' + artist + '</div>' +
+          '</a>' +
+          document.getElementById('favourites').innerHTML
+
+        if ((document.getElementById('favourite').src).endsWith('icons/favourite.svg')) {
+          document.getElementById('favourite').src = 'icons/favouritegold.svg'
+        } else if ((document.getElementById('favourite').src).endsWith('icons/favouritegold.svg')) {
+          document.getElementById('favourite').src = 'icons/favourite.svg'
+          $('.' + mbid).remove()
+        }
+
+        // Save new favourites
+        localStorage.setItem('favourites', document.getElementById('favourites').innerHTML)
+      })
+  }
+}
+
+function loadTopTracks () {
+  getTopTracks()
+    .then(function (data) {
+      var html = ''
+      for (var z = 0; z < 16; z++) {
+        var name = data.tracks.track[z].name
+        var artist = data.tracks.track[z].artist.name
+        var art = data.tracks.track[z].image[2]['#text']
+
+        var mbid = 'p' + makeID(artist, name)
+
+        html = html +
+          "<a class='card " + mbid + "' href='javascript:searchVideo(\"" + name.replace("'", '') + ' - ' + artist + "\")'>" +
+          "<img class='album' src='" + art + "'>" +
+          '<br>' +
+          "<div class='subtitle'>" + name + '<br>' + artist + '</div>' +
+          '</a>'
+      }
+      document.getElementById('popular').innerHTML = html
+    })
+}
+
+function makeID (artist, name) {
+  var conv = ''
+  var str = artist + name
+  for (var i = 0; i < str.length; i++) {
+    conv += str.charCodeAt(i)
+  }
+  conv = btoa(parseInt(conv))
+  conv = conv.replace(/=/g, '-')
+  return conv
 }
--- script.js
+++ script.js
@@ -1,265 +1,231 @@
-var tag = document.createElement('script');
+var tag = document.createElement('script')
 
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+tag.src = 'https://www.youtube.com/iframe_api'
+var firstScriptTag = document.getElementsByTagName('script')[0]
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
 
-var player;
+var player
 
-function onYouTubeIframeAPIReady() {
-	player = new YT.Player('player', {
-		height: '100%',
-		width: '100%',
-		events: {
-			'onReady': onPlayerReady,
-			'onStateChange': onPlayerStateChange
-		}
-	});
-
+function onYouTubeIframeAPIReady () {
+  player = new YT.Player('player', {
+    height: '100%',
+    width: '100%',
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  })
 }
 
-function onPlayerReady(event) {
-	var prog = document.getElementById("myBar");
-	var load = document.getElementById("loadBar");
+function onPlayerReady (event) {
+  var prog = document.getElementById('myBar')
+  var load = document.getElementById('loadBar')
 
-	player.playVideo()
+  player.playVideo()
 
-	var down = false;
+  var down = false
 
-	setInterval(function () {
+  setInterval(function () {
+    $(document).mousedown(function () {
+      down = true
+    }).mouseup(function () {
+      down = false
+      if (player.getPlayerState() === 2) {
+        prog.style.width = document.getElementById('progress').value / 1000 + '%'
+      }
+    })
 
-		$(document).mousedown(function () {
-			down = true;
-		}).mouseup(function () {
-			down = false;
-			if (player.getPlayerState() == 2) {
-				prog.style.width = document.getElementById("progress").value / 1000 + '%';
-			}
-
-		});
-
-		if (player.getPlayerState() == 1) {
-			prog.style.width = player.getCurrentTime() / player.getDuration() * 100 + '%';
-			load.style.width = player.getVideoLoadedFraction() * 100 + '%';
-			if (player.getVideoLoadedFraction() > 0 && (!down)) {
-				document.getElementById("progress").value = player.getCurrentTime() * 100000 / player.getDuration();
-			} else if (!down) {
-				document.getElementById("progress").value = 0;
-			}
-		}
-
-	}, 100);
-
+    if (player.getPlayerState() === 1) {
+      prog.style.width = player.getCurrentTime() / player.getDuration() * 100 + '%'
+      load.style.width = player.getVideoLoadedFraction() * 100 + '%'
+      if (player.getVideoLoadedFraction() > 0 && (!down)) {
+        document.getElementById('progress').value = player.getCurrentTime() * 100000 / player.getDuration()
+      } else if (!down) {
+        document.getElementById('progress').value = 0
+      }
+    }
+  }, 100)
 }
-var done = false;
 
-function onPlayerStateChange(event) {
-
-	if (player.getPlayerState() == 1) {
-		document.getElementById("playPauseButton").src = "icons/pause.svg"
-	}
+function onPlayerStateChange (event) {
+  if (player.getPlayerState() === 1) {
+    document.getElementById('playPauseButton').src = 'icons/pause.svg'
+  }
 
 
 
-	if (player.getPlayerState() == 0) {
-		player.stopVideo();
-		progressBar(0, 0, 0);
-		document.getElementById("album").src = "album.png";
-		document.getElementById("title").innerText = "";
-		document.getElementById("playPauseButton").src = "icons/play.svg"
+  if (player.getPlayerState() === 0) {
+    player.stopVideo()
+    progressBar(0, 0, 0)
+    document.getElementById('album').src = 'album.png'
+    document.getElementById('title').innerText = ''
+    document.getElementById('playPauseButton').src = 'icons/play.svg'
+  }
 
-	}
-
-	if (player.getPlayerState() == 5) {
-		document.getElementById("youtubeactive").style.pointerEvents = "none"
-		document.getElementById("youtube").style.pointerEvents = "none"
-		document.getElementById("clickShield").style.visibility = "hidden"
-		$('#player').fadeOut('fast', function () {
-			document.getElementById("player").style.visibility = "hidden"
-			$('#youtubeactive').fadeOut('fast', function () {
-				document.getElementById("youtubeactive").style.visibility = "hidden"
-				document.getElementById("youtubeactive").style.pointerEvents = "all"
-				document.getElementById("youtube").style.pointerEvents = "all"
-			});
-			document.getElementById("youtube").style.visibility = "visible"
-		});
-	}
-
+  if (player.getPlayerState() === 5) {
+    document.getElementById('youtubeactive').style.pointerEvents = 'none'
+    document.getElementById('youtube').style.pointerEvents = 'none'
+    document.getElementById('clickShield').style.visibility = 'hidden'
+    $('#player').fadeOut('fast', function () {
+      document.getElementById('player').style.visibility = 'hidden'
+      $('#youtubeactive').fadeOut('fast', function () {
+        document.getElementById('youtubeactive').style.visibility = 'hidden'
+        document.getElementById('youtubeactive').style.pointerEvents = 'all'
+        document.getElementById('youtube').style.pointerEvents = 'all'
+      })
+      document.getElementById('youtube').style.visibility = 'visible'
+    })
+  }
 }
 
 // This function defines the rules for the progress bar
-function progressBar(progress, loaded, total) {
-	var prog = document.getElementById("myBar");
-	var load = document.getElementById("loadBar");
-	var handle = document.getElementById("progress");
+function progressBar (progress, loaded, total) {
+  var prog = document.getElementById('myBar')
+  var load = document.getElementById('loadBar')
+  var handle = document.getElementById('progress')
 
-	prog.style.width = progress;
-	load.style.width = loaded;
-	handle.value = progress;
-
-
+  prog.style.width = progress
+  load.style.width = loaded
+  handle.value = progress
 }
 
 
 // These functions change video settings when called
-function fastForward() {
-	player.seekTo(player.getCurrentTime() + 10);
+function fastForward () {
+  player.seekTo(player.getCurrentTime() + 10)
 }
 
-function rewind() {
-	player.seekTo(player.getCurrentTime() - 10);
+function rewind () {
+  player.seekTo(player.getCurrentTime() - 10)
 }
 
-function volumeUp() {
-	player.setVolume(player.getVolume() + 10);
+function volumeUp () {
+  player.setVolume(player.getVolume() + 10)
 }
 
-function volumeDown() {
-	player.setVolume(player.getVolume() - 10);
+function volumeDown () {
+  player.setVolume(player.getVolume() - 10)
 }
 
-function seek(ts) {
-	player.seekTo(ts.value / 100000 * player.getDuration(), true);
+function seek (ts) {
+  player.seekTo(ts.value / 100000 * player.getDuration(), true)
 }
 
 // These functions toggle video settings and change textures
-function toggleMute() {
-	if (player.isMuted()) {
-		player.unMute()
-		document.getElementById("toggleMuteButton").src = "icons/loud.svg"
-	} else {
-		player.mute()
-		document.getElementById("toggleMuteButton").src = "icons/mute.svg"
-	}
+function toggleMute () {
+  if (player.isMuted()) {
+    player.unMute()
+    document.getElementById('toggleMuteButton').src = 'icons/loud.svg'
+  } else {
+    player.mute()
+    document.getElementById('toggleMuteButton').src = 'icons/mute.svg'
+  }
 }
 
-function togglePlay() {
-	if (player.getPlayerState() == 1) {
-		player.pauseVideo()
-		document.getElementById("playPauseButton").src = "icons/play.svg"
-	} else if (player.getPlayerState() == 2) {
-		player.playVideo()
-		document.getElementById("playPauseButton").src = "icons/pause.svg"
-	}
+function togglePlay () {
+  if (player.getPlayerState() === 1) {
+    player.pauseVideo()
+    document.getElementById('playPauseButton').src = 'icons/play.svg'
+  } else if (player.getPlayerState() === 2) {
+    player.playVideo()
+    document.getElementById('playPauseButton').src = 'icons/pause.svg'
+  }
 }
 
 
 // This function manages hotkeys
 
+
 document.onkeydown = function (e) {
-	cancel = false;
-	e = e || window.event;
+  e = e || window.event
 
-	if (!$("#search").is(":focus")){
-		if (e.keyCode == '32' || e.keyCode == '75') { // 'SPACE' (pause)
-			togglePlay()
-			return false;
-		}
-		if (e.keyCode == '37' || e.keyCode == '74') { // 'LEFT' (rewind)
-			rewind();
-		}
-		if (e.keyCode == '39' || e.keyCode == '76') { // 'RIGHT' (forward)
-			fastForward();
-		}
-		if (e.keyCode == '38') { // 'UP' (volume up)
-			volumeUp();
-		}
-		if (e.keyCode == '40') { // 'DOWN' (volume down)
-			volumeDown();
-		}
-		if (e.keyCode == '77'){ // 'M' (mute)
-			toggleMute();
-		}
-	}
-
-
+  if (!$('#search').is(':focus')) {
+    if (e.keyCode === 32 || e.keyCode === 75) { // 'SPACE' (pause)
+      togglePlay()
+      return false
+    }
+    if (e.keyCode === 37 || e.keyCode === 74) { // 'LEFT' (rewind)
+      rewind()
+    }
+    if (e.keyCode === 39 || e.keyCode === 76) { // 'RIGHT' (forward)
+      fastForward()
+    }
+    if (e.keyCode === 38) { // 'UP' (volume up)
+      volumeUp()
+    }
+    if (e.keyCode === 40) { // 'DOWN' (volume down)
+      volumeDown()
+    }
+    if (e.keyCode === 77) { // 'M' (mute)
+      toggleMute()
+    }
+  }
 }
-
-
 
 // This function takes a video search query as input
 // and changes the video to the first YouTube result
-function searchVideo(query) {
-	var prog = document.getElementById("myBar");
-	var load = document.getElementById("loadBar");
+function searchVideo (query) {
+  player.pauseVideo()
+  progressBar(0, 0, 0)
 
-	player.pauseVideo();
-	progressBar(0, 0, 0);
+  document.getElementById('playPauseButton').src = 'icons/play.svg'
 
-	document.getElementById("playPauseButton").src = "icons/play.svg"
+  loadSong(query)
 
-	loadSong(query);
+  console.log(query)
 
-	console.log(query);
-
-	searchTrack(query).then(function (data) {
-
-		player.loadPlaylist({
-			listType: "search",
-			list: data.results.trackmatches.track[0].name + " - " + data.results.trackmatches.track[0].artist,
-			index: 0
-		})
-	})
+  searchTrack(query).then(function (data) {
+    player.loadPlaylist({
+      listType: 'search',
+      list: data.results.trackmatches.track[0].name + ' - ' + data.results.trackmatches.track[0].artist,
+      index: 0
+    })
+  })
 }
 
 
-function clearHistory() {
-	localStorage.removeItem("history")
-	document.getElementById("history").innerHTML = '';
+function clearHistory () {
+  localStorage.removeItem('history')
+  document.getElementById('history').innerHTML = ''
 }
 
-async function imgsrcToB64(img) {
-	return new Promise((resolve, reject) => {
-		blobUtil.imgSrcToDataURL(img, 'image/png',
-			'Anonymous', 1.0).then(function (blob) {
-			resolve(blob)
-		}).catch(function (err) {
-			console.log(err) // error
-		});
-	})
+async function imgsrcToB64 (img) {
+  return new Promise((resolve, reject) => {
+    blobUtil.imgSrcToDataURL(img, 'image/png', 'Anonymous', 1.0).then(function (blob) {
+      resolve(blob)
+    }).catch(function (err) {
+      console.log(err) // error
+    })
+  })
 }
 
-function toggleVideo() {
+function toggleVideo () {
+  if (document.getElementById('player').style.visibility === 'hidden' && (player.getPlayerState() === 1 || player.getPlayerState() === 2)) {
+    document.getElementById('youtubeactive').style.pointerEvents = 'none'
+    document.getElementById('youtube').style.pointerEvents = 'none'
+    document.getElementById('player').style.visibility = 'visible'
+    document.getElementById('youtubeactive').style.visibility = 'visible'
+    $('#player').fadeIn('fast', function () {
+      document.getElementById('clickShield').style.visibility = 'visible'
+    })
+    $('#youtubeactive').fadeIn('fast', function () {
+      document.getElementById('youtube').style.visibility = 'hidden'
+      document.getElementById('youtubeactive').style.pointerEvents = 'all'
+      document.getElementById('youtube').style.pointerEvents = 'all'
+    })
+  } else if (player.getPlayerState() >= 0) {
+    document.getElementById('youtubeactive').style.pointerEvents = 'none'
+    document.getElementById('youtube').style.pointerEvents = 'none'
+    document.getElementById('clickShield').style.visibility = 'hidden'
+    $('#player').fadeOut('fast', function () {
+      document.getElementById('player').style.visibility = 'hidden'
 
-	if (document.getElementById("player").style.visibility == "hidden" && (player.getPlayerState() == 1 || player.getPlayerState() == 2)) {
-		document.getElementById("youtubeactive").style.pointerEvents = "none"
-		document.getElementById("youtube").style.pointerEvents = "none"
-		document.getElementById("player").style.visibility = "visible"
-		document.getElementById("youtubeactive").style.visibility = "visible"
-		$('#player').fadeIn('fast', function () {
-			document.getElementById("clickShield").style.visibility = "visible";
-		});
-		$('#youtubeactive').fadeIn('fast', function () {
-			document.getElementById("youtube").style.visibility = "hidden"
-			document.getElementById("youtubeactive").style.pointerEvents = "all"
-			document.getElementById("youtube").style.pointerEvents = "all"
-		});
-
-
-	} else if (player.getPlayerState() >= 0) {
-		document.getElementById("youtubeactive").style.pointerEvents = "none"
-		document.getElementById("youtube").style.pointerEvents = "none"
-		document.getElementById("clickShield").style.visibility = "hidden"
-		$('#player').fadeOut('fast', function () {
-			document.getElementById("player").style.visibility = "hidden"
-
-			$('#youtubeactive').fadeOut('fast', function () {
-				document.getElementById("youtubeactive").style.visibility = "hidden"
-				document.getElementById("youtubeactive").style.pointerEvents = "all"
-				document.getElementById("youtube").style.pointerEvents = "all"
-			});
-			document.getElementById("youtube").style.visibility = "visible"
-		});
-	}
-}
-
-function makeID(artist, name) {
-	conv = ""
-	str = artist + name;
-	for (i = 0; i < str.length; i++) {
-		conv += str.charCodeAt(i)
-	}
-	conv = btoa(parseInt(conv));
-	conv = conv.replace(/=/g, "-");
-	return conv;
+      $('#youtubeactive').fadeOut('fast', function () {
+        document.getElementById('youtubeactive').style.visibility = 'hidden'
+        document.getElementById('youtubeactive').style.pointerEvents = 'all'
+        document.getElementById('youtube').style.pointerEvents = 'all'
+      })
+      document.getElementById('youtube').style.visibility = 'visible'
+    })
+  }
 }
